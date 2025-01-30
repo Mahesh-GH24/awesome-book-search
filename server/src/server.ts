fc9364272819d7927c289bdd6ad7ab41e3181c61
Mahesh Gunasekaran <maheshgunasekaran24@gmail.com>
2025-01-29 22:38:57 -0600

    server changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -45,10 +45,11 @@ const startApolloServer = async () => {
 
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
+    app.use(express.static(path.join(__dirname,'../../')))
     
     app.get('*', (_req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
 
