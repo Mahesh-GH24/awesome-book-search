42f8e4703c139b1e2441869214fb2555e85f9d23
Mahesh Gunasekaran <maheshgunasekaran24@gmail.com>
2025-01-29 21:49:51 -0600

    pkg.json changes-additional

--- server/src/server.ts
+++ server/src/server.ts
@@ -46,7 +46,7 @@ const startApolloServer = async () => {
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
-
+    
     app.get('*', (_req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
